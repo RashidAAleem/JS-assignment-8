bd94c36fb9b71725ed0c7573587ac7507cb8b255
Rana Rashid <124416215+RashidAAleem@users.noreply.github.com>
2024-09-28 22:11:16 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
+// //ascending order
+
+// // var number = [24, 53, 78, 91, 12, 152, 125];
+// // for (var i = 0; i < number.length; i++) {
+// // for (var j = 0; j < number.length; j++) {
+// //     if (number[j]> number[j+1]) {
+// //         [number[j], number[j+1]] = [number[j+1], number [j]]
+        
+// //     }
+    
+// // }
+    
+
+    
+    
+// // }
+// // document.write(number); 
+
+
+// // descending order
+// // var number1 = [24, 53, 78, 91, 12, 152, 125];
+// // for (var i = 0; i < number1.length; i++) {
+// // for (var j = 0; j < number1.length; j++) {
+// //     if (number1[j]< number1[j+1]) {
+// //         [number1[j], number1[j+1]] = [number1[j+1], number1 [j]]
+        
+// //     }
+    
+// // }
+// // }
+
+// // document.write(number1); 
 
